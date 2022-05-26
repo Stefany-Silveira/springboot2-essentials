66cd5abfcdc7dd1c9157b822fb917b620b49dda3
Stefany Silveira <stefany.silveira21@gmail.com>
2022-05-25 23:08:13 -0300

    create @método POST

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -5,22 +5,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L,"Berserk"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk")));
+    }
 
     //private final AnimeRepository animeRepository
     public List<Anime> listAll() {
         return animes;
     }
 
-    public Anime findById(long id)  {
-
+    public Anime findById(long id) {
         return animes.stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime Not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId (ThreadLocalRandom.current().nextLong(3,100000));
+        animes.add(anime);
+        return anime;
+
     }
 }
