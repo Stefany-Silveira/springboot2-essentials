c665f0a27d06d9f97f5720394344e52d2c9fd69f
stefanysilveira <stefany.silveira21@gmail.com>
2022-06-13 21:25:59 -0300

    import @ lombok

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,14 +1,15 @@
 package academy.devdojo.springboot2.domain;
 
-import lombok.*;
-import org.hibernate.Hibernate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
-import java.util.Objects;
 
 @Data
 @AllArgsConstructor
